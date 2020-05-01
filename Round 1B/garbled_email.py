eb6db9d39206cdb457ff145de3904ab77c92f0e7
Allen Liu <kamyu104@gmail.com>
2020-05-02 05:48:59 +0800

    update

--- Round 1B/garbled_email.py
+++ Round 1B/garbled_email.py
@@ -20,20 +20,20 @@ def garbled_email():
                 word = S[i:i+l]
                 # no change
                 if word in LOOKUP:
-                    prev = max(j-len(word), -D)  # merge states
+                    prev = max(j-l, -D)  # merge states
                     dp[i+l][prev] = min(dp[i+l][prev], dp[i][j])
                 # one change
-                for k in xrange(j+D, len(word)):
+                for k in xrange(j+D, l):
                     if word[:k]+'*'+word[k+1:] not in LOOKUP:
                         continue
-                    prev = max(k-len(word), -D)  # merge states
+                    prev = max(k-l, -D)  # merge states
                     dp[i+l][prev] = min(dp[i+l][prev], dp[i][j]+1)
                 # two changes
-                for k in xrange(j+D, len(word)):
-                    for m in xrange(k+D, len(word)):
+                for k in xrange(j+D, l):
+                    for m in xrange(k+D, l):
                         if word[:k]+'*'+word[k+1:m]+'*'+word[m+1:] not in LOOKUP:
                             continue
-                        prev = max(m-len(word), -D)  # merge states
+                        prev = max(m-l, -D)  # merge states
                         dp[i+l][prev] = min(dp[i+l][prev], dp[i][j]+2)
     return min(dp[-1])
 
