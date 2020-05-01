bb5f2cccf1ffddcf7784d8bf80624e68dd931fc3
Allen Liu <kamyu104@gmail.com>
2020-05-01 23:42:44 +0800

    update

---
+++ Round 1B/osmos.py
@@ -0,0 +1,26 @@
+# Copyright (c) 2020 kamyu. All rights reserved.
+#
+# Google Code Jam 2013 Round 1B - Problem A. Osmos
+# https://code.google.com/codejam/contest/2434486/dashboard#s=p0
+#
+# Time:  O(NlogN)
+# Space: O(1)
+#
+
+def osmos():
+    curr, N = map(int, raw_input().strip().split())
+    motes = sorted(map(int, raw_input().strip().split()))
+    result = 0
+    for i, mote in enumerate(motes):
+        for _ in xrange(N - max(i, result)):
+            if curr > mote:
+                curr += mote
+                break
+            curr += curr-1
+            result += 1
+        else:
+            break
+    return result        
+
+for case in xrange(input()):
+    print 'Case #%d: %s' % (case+1, osmos())
